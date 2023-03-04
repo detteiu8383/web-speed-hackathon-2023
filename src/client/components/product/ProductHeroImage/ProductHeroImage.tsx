3551da82333523683c128819c14ec98d69720b4f
d_etteiu8383 <ysdr83@gmail.com>
2023-03-04 15:26:40 +0900

    lodashの削除

--- src/client/components/product/ProductHeroImage/ProductHeroImage.tsx
+++ src/client/components/product/ProductHeroImage/ProductHeroImage.tsx
@@ -1,5 +1,4 @@
 import classNames from 'classnames';
-import _ from 'lodash';
 import { memo } from 'react';
 import type { FC } from 'react';
 
@@ -61,6 +60,6 @@ export const ProductHeroImage: FC<Props> = memo(({ product, title }) => {
       }}
     </GetDeviceType>
   );
-}, _.isEqual);
+});
 
 ProductHeroImage.displayName = 'ProductHeroImage';
