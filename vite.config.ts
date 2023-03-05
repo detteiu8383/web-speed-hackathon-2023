ad88874adfb2ac4e6f0eb718002aa85e0be87154
d_etteiu8383 <ysdr83@gmail.com>
2023-03-05 09:15:26 +0900

    vite-plugin-ejsの削除

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,10 @@
-import path from 'node:path';
-
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
-import { ViteEjsPlugin } from 'vite-plugin-ejs';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import wasm from 'vite-plugin-wasm';
 
-import { getFileList } from './tools/get_file_list';
-
-const publicDir = path.resolve(__dirname, './public');
-const getPublicFileList = async (targetPath: string) => {
-  const filePaths = await getFileList(targetPath);
-  const publicFiles = filePaths
-    .map((filePath) => path.relative(publicDir, filePath))
-    .map((filePath) => path.join('/', filePath));
-
-  return publicFiles;
-};
-
 export default defineConfig(async ({ mode }) => {
-  const videos = await getPublicFileList(path.resolve(publicDir, 'videos'));
-
   return {
     build: {
       assetsInlineLimit: 20480,
@@ -43,15 +26,6 @@ export default defineConfig(async ({ mode }) => {
       },
       target: 'chrome110',
     },
-    plugins: [
-      react(),
-      wasm(),
-      topLevelAwait(),
-      ViteEjsPlugin({
-        module: '/src/client/index.tsx',
-        title: '買えるオーガニック',
-        videos,
-      }),
-    ],
+    plugins: [react(), wasm(), topLevelAwait()],
   };
 });
